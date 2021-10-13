865ab1210b5eb9cac62dc95b6f64ce338f5b5566
Christos Kontorias <chriskavala1999@gmail.com>
2021-10-13 19:24:07 +0300

    Add Project Files

---
+++ app/src/main/java/com/example/application/LoginActivity.java
@@ -0,0 +1,115 @@
+package com.example.application;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TextView createNewAccount;
+    EditText inputEmail,inputPassword;
+    Button btnLogin;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    ProgressDialog progressDialog;
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+    ImageView btnGoogle;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        //Remove Top Bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        createNewAccount = findViewById(R.id.createNewAccount);
+        inputEmail=findViewById(R.id.inputEmail);
+        btnLogin=findViewById(R.id.btnLogin);
+        inputPassword=findViewById(R.id.inputPassword);
+        progressDialog=new ProgressDialog(this);
+        mAuth=FirebaseAuth.getInstance();
+        mUser=mAuth.getCurrentUser();
+        btnGoogle=findViewById(R.id.btnGoogle);
+
+        createNewAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                perforLogin();
+            }
+        });
+
+        btnGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, GoogleSignInActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void perforLogin() {
+        String email=inputEmail.getText().toString();
+        String password=inputPassword.getText().toString();
+
+        if (!email.matches(emailPattern))
+        {
+            inputEmail.setError("This Field Cannot Be Empty!");
+        }else if(password.isEmpty() || password.length()<6)
+        {
+            inputPassword.setError("Please Enter A Valid Password");
+        }else {
+            progressDialog.setMessage("Please Wait While Login...");
+            progressDialog.setTitle("Login");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful())
+                    {
+                        progressDialog.dismiss();
+                        sendUserToNextActivity();
+                        Toast.makeText(LoginActivity.this,"Login Successful", Toast.LENGTH_SHORT).show();
+                    }else
+                    {
+                        progressDialog.dismiss();
+                        Toast.makeText(LoginActivity.this,""+task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private void sendUserToNextActivity() {
+        Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/application/RegisterActivity.java
@@ -0,0 +1,110 @@
+package com.example.application;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    TextView AlreadyHaveAnAccount;
+    EditText inputEmail,inputPassword,inputConfirmPassword;
+    Button btnRegister;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    ProgressDialog  progressDialog;
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        //Remove Top Bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        AlreadyHaveAnAccount = findViewById(R.id.AlreadyHaveAnAccount);
+
+        inputEmail=findViewById(R.id.inputEmail);
+        inputPassword=findViewById(R.id.inputPassword);
+        inputConfirmPassword=findViewById(R.id.inputConfirmPassword);
+        btnRegister=findViewById(R.id.btnRegister);
+        progressDialog=new ProgressDialog(this);
+        mAuth=FirebaseAuth.getInstance();
+        mUser=mAuth.getCurrentUser();
+
+        AlreadyHaveAnAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PerforAuth();
+            }
+        });
+    }
+
+    private void PerforAuth() {
+        String email=inputEmail.getText().toString();
+        String password=inputPassword.getText().toString();
+        String confirmPassword=inputConfirmPassword.getText().toString();
+
+        if (!email.matches(emailPattern))
+        {
+            inputEmail.setError("This Field Cannot Be Empty!");
+        }else if(password.isEmpty() || password.length()<6)
+        {
+            inputPassword.setError("Please Enter A Valid Password");
+        }else if (!password.equals(confirmPassword))
+        {
+            inputConfirmPassword.setError("Password Not Match Both Field");
+        }else
+        {
+            progressDialog.setMessage("Please Wait While Registration...");
+            progressDialog.setTitle("Registration");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful())
+                    {
+                        progressDialog.dismiss();
+                        sendUserToNextActivity();
+                        Toast.makeText(RegisterActivity.this,"Registration Successful", Toast.LENGTH_SHORT).show();
+                    }else
+                    {
+                        progressDialog.dismiss();
+                        Toast.makeText(RegisterActivity.this,""+task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private void sendUserToNextActivity() {
+        Intent intent = new Intent(RegisterActivity.this,MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+}
